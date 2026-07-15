23d2ed111c83f17195d7c095783d01c299bfbae0
Alisson <alisson@yas.app>
2026-07-15 19:46:04 -0300

    🔧 Fix IP, compileSdk 37, update deps

--- android/YasApp/app/src/main/java/com/yas/api/RetrofitClient.java
+++ android/YasApp/app/src/main/java/com/yas/api/RetrofitClient.java
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClient {
 
     // ⚠️ Mude para o IP externo da sua VM quando testar no celular
-    private static final String BASE_URL = "http://10.0.2.2:8080/";
+    private static final String BASE_URL = "http://34.70.145.11:8080/";
 
     private static Retrofit retrofit = null;
 
